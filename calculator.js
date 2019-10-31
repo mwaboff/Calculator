ecf29e27543c95b33292aeb8e5b909394dabc2c4
Michael Aboff <mwaboff@gmail.com>
2019-10-31 01:16:50 -0400

    first commit, working basic calculator

---
+++ calculator.js
@@ -0,0 +1,178 @@
+(function() {
+  
+  var myCalculator = window.myCalculator = (window.myCalculator || {});
+
+  var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+  var operators = ["+","-","*","/","="];
+  var current_expression = "";
+  var total = 0;
+
+
+  myCalculator.initialize = function() {
+    addButtons();
+    updateCurrentExpression();
+    updateTotal(0);
+  }
+
+
+  var addButtons = function() {
+    var parent = document.getElementById("buttons");
+    var options = numbers.concat(operators);
+    for(var i = 0; i < options.length; i++) {
+      let new_button = document.createElement("div");
+      new_button.setAttribute("class", "calc_button");
+      new_button.setAttribute("id", "button_"+options[i]);
+      new_button.setAttribute("value", options[i]);
+      new_button.innerText = options[i];
+      parent.appendChild(new_button);
+      new_button.addEventListener("click", myCalculator.buttonClick)
+    }
+  }
+
+
+  myCalculator.buttonClick = function(event) {
+    var button = event.target
+    var value = button.getAttribute("value")
+
+    if (!isNaN(value)) {
+      // isNaN will return false if the value is NOT a number. Thus !isNan will
+      // return true if it is a number.
+
+      addNumber(value);
+    } else if (value === "=") {
+      evaluateCurrentExpression();
+    } else {
+      addOperand(value);
+    }
+  };
+
+
+  var addNumber = function(value) {
+    current_expression += value;
+    updateCurrentExpression();
+  };
+
+
+  var addOperand = function(value) {
+    if (operators.includes(current_expression.charAt(current_expression.length - 2))) {
+      // Prevent adding two operators in a row.
+      return;
+    }
+
+    if (current_expression === "") {
+      current_expression += total.toString();
+    }
+
+    current_expression += " " + value + " ";
+    updateCurrentExpression();
+  };
+
+
+  var updateCurrentExpression = function() {
+    var updateText = current_expression;
+    if (current_expression === "") {
+      updateText = "0";
+    }
+    document.getElementById("curr_expression").innerText = updateText;
+  };
+
+
+  var updateTotal = function(new_total) {
+    total = new_total;
+    document.getElementById("total").innerText = total;
+  }
+
+
+  var evaluateCurrentExpression = function() {
+    expr_arr = current_expression.split(" ");
+
+    if (!checkValidity(expr_arr)) {
+      return;
+    }
+
+    let new_total = orderOfOperations(expr_arr);
+
+    updateTotal(new_total);
+    current_expression = "";
+    updateCurrentExpression();
+  };
+
+  var checkValidity = function(expr_arr) {
+    if (expr_arr.length === 0) {
+      console.log("Error: Empty equation.");
+      return false;
+    }
+
+    if (operators.includes(expr_arr[0])) {
+      console.log("Error: Operators cannot start the equation.");
+      return false;
+    }
+
+    if (operators.includes(expr_arr[expr_arr.length-1])) {
+      console.log("Error: operators cannot end be at the end of the equation");
+      return false;
+    }
+
+    return true;
+  }
+
+  var orderOfOperations = function(expr_arr) {
+    expr_arr = handleMD(expr_arr); // Handle multiplication and division
+    expr_arr = handleAS(expr_arr); // handle addition and subtraction
+
+    return expr_arr[0]; // There should only be one value left, that is the total.
+  }
+
+  var handleMD = function(expr_arr) {
+    // We will loop through each element. If we see a * or /, we will perform the
+    // math on the numbers on either side. We are hoping that there is a value
+    // on both sides. If not, substitute it with the value 1.
+
+    var md = ['*', '/'];
+
+    for (var i = 0; i < expr_arr.length-1; i++) {
+      if (md.includes(expr_arr[i])) {
+        var temp_total;
+        let first_num = parseInt(expr_arr[i-1]);
+        let second_num = parseInt(expr_arr[i+1]);
+
+        if (expr_arr[i] === "*") {
+          temp_total = first_num * second_num;
+        } else {
+          temp_total = first_num / second_num;
+        }
+
+        expr_arr.splice(i-1, 3, temp_total); // Remove the three elements and insert total
+
+        i -= 2; // Adjust for the two removed values.
+
+      }
+    }
+
+    return expr_arr;
+  }
+
+  var handleAS = function(expr_arr) {
+    var as = ['+', '-'];
+
+    for (var i = 0; i < expr_arr.length-1; i++) {
+      if (as.includes(expr_arr[i])) {
+        var temp_total;
+        let first_num = parseInt(expr_arr[i-1]);
+        let second_num = parseInt(expr_arr[i+1]);
+
+        if (expr_arr[i] === "+") {
+            temp_total = first_num + second_num;
+        } else {
+            temp_total = first_num - second_num;
+        }
+
+        expr_arr.splice(i-1, 3, temp_total); // Remove the three elements and insert total
+
+        i -= 2; // Adjust for the two removed values.
+
+      }
+    }
+    return expr_arr;
+  }
+})();
